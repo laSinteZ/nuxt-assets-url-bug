c15c11135362a27e31725cb0b00cd6d9eab3fc9f
Vyacheslav Bikbaev <la.sintez@gmail.com>
2018-09-21 15:32:45 +0300

    fix: actual info

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,11 @@
 <template>
-  <section style="display: flex">
-    <div>img src is resolved by webpack<img src="~/assets/square.svg" /></div>
-    <div class="square-not-visible-1">URL is not resolved by webpack</div>
-    <div class="square-not-visible-2">URL is not resolved by webpack</div>
-    <div class="square-visible-1">Image is served from static</div>
-    <div class="square-visible-2">Image is served from static</div>
+  <section style="display: flex;">
+    <div>img src is resolved by webpack ~/assets/<img src="~/assets/square.svg" /></div>
+    <div>img src is resolved by webpack ~assets/<img src="~assets/square.svg" /></div>
+    <div class="square-not-visible-1">URL is not resolved by webpack~/assets/</div>
+    <div class="square-not-visible-2">URL is not resolved by webpack ~/assets/</div>
+    <div class="square-visible-1">URL is resolved by webpack ~assets/</div>
+    <div class="square-visible-2">URL is resolved by webpack ~assets/</div>
   </section>
 </template>
 
@@ -17,16 +18,16 @@
 .square-not-visible-2 {
   min-width: 300px;
   min-height: 300px;
-  background-image: url('/assets/square.svg');
+  background-image: url('~/assets/square.svg');
 }
 .square-visible-1 {
   min-width: 300px;
   min-height: 300px;
-  background-image: url(/square.svg);
+  background-image: url(~assets/square.svg);
 }
 .square-visible-2 {
   min-width: 300px;
   min-height: 300px;
-  background-image: url('/square.svg');
+  background-image: url('~assets/square.svg');
 }
 </style>
